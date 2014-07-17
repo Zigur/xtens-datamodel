a05b32723ead8e449ac4770850a14e33fa88e172
massi <massi@laptop21.(none)>
2014-07-17 18:04:56 +0200

    composite data type implementation

---
+++ test/unit/DataType-test.js
@@ -0,0 +1,18 @@
+/**
+ * Unit test package for the DataType class
+ *
+ */
+describe("CompositeDataType", function() {
+    
+    /* CompositeDataType constructor test */
+    describe("#constructor()", function(){
+        //TODO: test goes here
+    });
+
+    /* add() function test */
+    describe(".#add()", function() {
+        it("should add a MetadataGroup object to a CompositeDataType", function() {
+            //TODO: test goes here
+        });
+    });
+});
