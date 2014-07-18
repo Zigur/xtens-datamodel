21139938bb12c9f6c9c79030a0faaad890a9642b
massi <massi@laptop21.(none)>
2014-07-18 17:28:37 +0200

    Composite Data Type implemented

--- test/unit/DataType-test.js
+++ test/unit/DataType-test.js
@@ -2,11 +2,22 @@
  * Unit test package for the DataType class
  *
  */
+var should = require('should');
+var CompositeDataType = require('../../lib/DataType.js').CompositeDataType;
+
 describe("CompositeDataType", function() {
     
     /* CompositeDataType constructor test */
     describe("#constructor()", function(){
-        //TODO: test goes here
+        it("should create an empty DataType", function() {
+            var id = 1;
+            var name = "Test Data Type";
+            var dataType = new CompositeDataType(id, name);
+            dataType.id.should.be.exactly(id);
+            dataType.name.should.equal(name);
+            dataType.header.should.be.empty;
+            dataType.body.should.be.empty;
+        });
     });
 
     /* add() function test */
