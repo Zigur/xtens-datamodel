4fa5326dba2671f1ffb2ac7ac708f25dcac0ff46
massi <massi@laptop21.(none)>
2014-07-18 18:27:43 +0200

    MetadataField implemented

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ xtens-datamodel
 ===============
 
 A Javascript implementation of the XTENS data model for heterogeneous data management in Biomedical Science
+
+DataType-test -> TODO: add assertion on mdataField
--- test/unit/DataType-test.js
+++ test/unit/DataType-test.js
@@ -4,9 +4,39 @@
  */
 var should = require('should');
 var CompositeDataType = require('../../lib/DataType.js').CompositeDataType;
+var MetaDataField = require('../../lib/DataType.js').MetaDataField;
+
+/**
+ *METADATA_FIELD UNIT TESTS
+ *
+ */
+describe("MetaDataField", function() {
+
+    /* MetadataField constructor unit tests */
+    describe("#constructor()", function() {
+        it("should create a full-fledged STRING MetaDataField", function() {
+            var object = {};
+            object.name = 'Noldorian Kings';
+            object.required = true;
+            object.isList = true;
+            object.possibleValues = ['Fingolfin', 'Finarfin', 'Feanor', 'Finwe', 'Turgon', 'Gil-galad'];
+            object.hasUnit = true;  // I'm faking languages as units
+            object.possibleUnits = ['Quenya', 'Sindarin'];
+            var mdataField = new MetaDataField(params);
+            /* TODO add assertion on mdataField */
+        });
+    });
+
+});
+
+
+/**
+ *COMPOSITE_DATA_TYPE UNIT TESTS
+ *
+ */
 
 describe("CompositeDataType", function() {
-    
+
     /* CompositeDataType constructor test */
     describe("#constructor()", function(){
         it("should create an empty DataType", function() {
@@ -27,3 +57,5 @@ describe("CompositeDataType", function() {
         });
     });
 });
+
+
