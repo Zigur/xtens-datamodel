5ceb7956b2ccdfbc2b18a0c6dd5aaf113b7a744e
massi <massi@laptop21.(none)>
2014-07-22 19:07:36 +0200

    implemented common composite metadata class

--- test/unit/DataType-test.js
+++ test/unit/DataType-test.js
@@ -1,10 +1,14 @@
 /**
- * Unit test package for the DataType class
+ * @fileOverview Unit test package for the DataType class
+ * @author Massimiliano Izzo
+ * @version: 0.1
  *
  */
 var should = require('should');
 var Constants = require('../../lib/Constants.js').Constants;
-var CompositeDataType = require('../../lib/DataType.js').CompositeDataType;
+var DataType = require('../../lib/DataType.js').DataType;
+var MetadataGroup = require('../../lib/DataType.js').MetadataGroup;
+var MetadataLoop = require('../../lib/DataType.js').MetadataLoop;
 var MetadataField = require('../../lib/DataType.js').MetadataField;
 
 /**
@@ -24,7 +28,6 @@ describe("MetaDataField", function() {
             object.hasUnit = true;  // I'm faking languages as units
             object.possibleUnits = ['Quenya', 'Sindarin'];
             var mdataField = new MetadataField(object);
-            console.log(mdataField.getLabel());
             mdataField.getLabel().should.equal(Constants.METADATA_FIELD);
             mdataField.name.should.equal(object.name);
             mdataField.required.should.be.true;
@@ -32,7 +35,7 @@ describe("MetaDataField", function() {
             var len = object.possibleValues.length;
             mdataField.possibleValues.should.have.length(len);
             for (var i=0; i<len; i++) {
-                 mdataField.possibleValues[i].should.equal(object.possibleValues[i]);
+                mdataField.possibleValues[i].should.equal(object.possibleValues[i]);
             }
             mdataField.hasUnit.should.be.true;
             len = object.possibleUnits.length;
@@ -51,27 +54,69 @@ describe("MetaDataField", function() {
  *
  */
 
-describe("CompositeDataType", function() {
+describe("DataType", function() {
 
-    /* CompositeDataType constructor test */
+    /* DataType constructor test */
     describe("#constructor()", function(){
         it("should create an empty DataType", function() {
             var id = 1;
             var name = "Test Data Type";
-            var dataType = new CompositeDataType(id, name);
+            var dataType = new DataType(id, name);
             dataType.id.should.be.exactly(id);
             dataType.name.should.equal(name);
             dataType.header.should.be.empty;
-            dataType.body.should.be.empty;
+            dataType.content.should.be.empty;
         });
     });
 
     /* add() function test */
     describe(".#add()", function() {
-        it("should add a MetadataGroup object to a CompositeDataType", function() {
-            //TODO: test goes here
+        it("should add a MetadataGroup object to a DataType", function() {
+            var dataType = new DataType(1, "Test Data Type");
+            var mdataGroups = [], MAX = 5;
+            for (var i=0; i<MAX; i++) {
+                mdataGroups[i] = new MetadataGroup(i, "testGroup_"+i);
+            }
+            for (i=MAX-1; i>=0; i--) {
+                dataType.add(mdataGroups[i]);
+            }
+            dataType.content.should.have.lengthOf(5);
+            for (i=0; i<MAX; i++) {
+                dataType.content[i].should.have.ownProperty('id').equal(MAX-1-i);
+                dataType.content[i].should.have.ownProperty('name').equal("testGroup_"+(MAX-1-i));
+            }
         });
     });
 });
 
+/**
+ * @method
+ * unit test methods for the MetadataLoop class
+ *
+ */
+describe("MetadataLoop", function() {
+    describe("#constructor", function() {
+        it("should create an empty MetadataLoop", function() {
+            var id = 1;
+            var loop = new MetadataLoop(id);
+            loop.id.should.be.exactly(id);
+            loop.getLabel().should.equal(Constants.METADATA_LOOP);
+            loop.content.should.be.instanceOf(Array);
+            loop.content.should.be.empty;
+        });  
+    });
 
+    describe("#add", function() {
+        it("should throw an error", function(done) {
+            var loop_1 = new MetadataLoop(1);
+            var loop_2 = new MetadataLoop(2);
+            try {
+                loop_1.add(loop_2);
+                assert.fail();
+            }
+            catch(e) {
+                done();
+            } 
+        });
+    });
+});
